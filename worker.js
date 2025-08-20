88d021ddfde7ffb410820525ba90e58d99688d83
charu1603 <kakdechaitrali16@gmail.com>
2025-08-20 12:43:28 +0530

    changed url

--- worker.js
+++ worker.js
@@ -36,8 +36,9 @@ const worker = new Worker(
   {
     concurrency: 100,
     connection: {
-      url: process.env.UPSTASH_REDIS_URL,
-      token: process.env.UPSTASH_REDIS_REST_TOKEN
+      url: process.env.UPSTASH_REDIS_REST_URL,
+      token: process.env.UPSTASH_REDIS_REST_TOKEN,
+     
     },
   }
 );
